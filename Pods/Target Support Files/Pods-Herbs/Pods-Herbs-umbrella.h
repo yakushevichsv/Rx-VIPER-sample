d2da25dc2d9b38e442e9a9f0efcb2956a968b1eb
Siarhei Yakushevich <yakushevichsv@hotmail.com>
2016-12-15 17:37:17 +0100

    Removed Pods

--- Pods/Target Support Files/Pods-Herbs/Pods-Herbs-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_HerbsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_HerbsVersionString[];
-
--- Pods/Target Support Files/Pods-HerbsTests/Pods-HerbsTests-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double Pods_HerbsTestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_HerbsTestsVersionString[];
-
